fd7e89e7e81c180e61152d52c211cc9332f917aa
viqtechnologies <viqtechnologies@gmail.com>
2026-09-25 16:19:08 +0530

    Update Leaders.jsx

--- frontend/src/components/Leaders.jsx
+++ frontend/src/components/Leaders.jsx
@@ -280,7 +280,7 @@ const directors = [
     img: director2,
     linkedin: "#",
     description:
-    "Vishal Sharma is a Director at VIQ Technologies with 20+ years of professional experience in Sales, Marketing, Education Consultancy, and Career Counselling. He has established strong associations with *colleges and universities across Punjab, Haryana, Himachal Pradesh, Uttarakhand, Rajasthan, Jammu & Kashmir, and Bangalore. His expertise in the education sector focuses on **strategic partnerships, student guidance, and career-oriented solutions.",
+    "Vishal Sharma is a Director at VIQ Technologies with 20+ years of professional experience in Sales, Marketing, Education Consultancy, and Career Counselling. He has established strong associations with colleges and universities across Punjab, Haryana, Himachal Pradesh, Uttarakhand, Rajasthan, Jammu & Kashmir, and Bangalore. His expertise in the education sector focuses on strategic partnerships, student guidance, and career-oriented solutions.",
       tagline: ["Leadership", "Strategy", "Vision"],
   },
  
