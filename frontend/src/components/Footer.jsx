643191ad124912163f60074f44e41b603dbda6e9
viqtechnologies <viqtechnologies@gmail.com>
2026-09-25 16:26:02 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -65,7 +65,7 @@ const Footer = () => {
     },
     {
       icon: "fab fa-linkedin-in",
-      href: "https://www.linkedin.com/",
+      href: "https://www.linkedin.com/company/viqtechnologies/",
       label: "LinkedIn",
     },
     {
